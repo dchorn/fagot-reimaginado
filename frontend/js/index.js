9d745d86912e724674213c8ccbeb678eb67beb99
Pau Figueras <pau@pswsm.cat>
2022-12-06 22:43:05 +0100

    A lot of frontend work that did basically nothing to the UI

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -7,47 +7,84 @@ function humanize(str) {
 }
 
 function reqOk(data) {
+	const headers = Object.keys(data[0]);
+	// Regexes
+	const date_regex = /\d{4}-\d{2}-\d{2}/;
+	const price_regex = /\d+\.\d+/;
+
+	// Prices and genres
+	const gameGenres = Object.keys(Genere);
+	const gamePrices = Object.keys(ClasePreu);
+
 	console.log('data:', data);
 	$('#buttons').css('display', 'none');
-	let table = '<table class="table"><thead><tr>';
-	let headers = Object.keys(data[0]);
+	// Clear headers
 	let printableHeaders = [];
-	let date_regex = /\d{4}-\d{2}-\d{2}/;
-	const price_regex = /\d+\.\d+/;
 	headers.forEach(header => {
 		printableHeaders.push(humanize(header));
 	});
-	// console.log('headers:', printableHeaders)
+
+	let table = '<form id="updateF"><table class="table"><thead><tr>';
 	printableHeaders.forEach(header => {
 		table += '<th>' + header + '</th>';
 	})
 	table += '</thead><tbody>';
 
+	let gameData = [];
+
 	data.forEach(game => {
+		let curGame = new Game(game.id, game.nom, game.preu, game.clase_preu, game.genere, game["data_llançament"])
+		gameData.push(curGame);
+	});
+
+	console.log('gameData:', gameData)
+	gameData.forEach(game => {
 		table += '<tr>';
 		for (const game_key in game) {
 			if (game.hasOwnProperty(game_key)) {
 				table += '<td>';
-				if (date_regex.test(game[game_key])) {
-					table += `<input type="text" id="date" value="${game[game_key]}">`;
+				if (game[game_key] instanceof Date) {
+					table += `<input type="text" id="${game.nom}Date" name="date" value="${game[game_key].toISOString().slice(0,10)}">`;
 				} else if (price_regex.test(game[game_key])) {
-					table += `<input type="number" value="${game[game_key]}">`;
+					table += `<input type="number" name="${game.nom}Price" value="${game[game_key]}">`;
 				} else {
-					table += `<input type="text" value="${game[game_key]}">`
+					table += `<input type="text" name="${game.nom}${game_key}" value="${game[game_key]}">`
 				}
 				table += '</td>';
 			}
 		}
-		table += '</tr>';
+		table += `<td><button id="${game.nom}Update" disabled>Update</button></td></tr>`;
 	});
-	table += '</tbody></table>';
+	table += '</tbody></table></form>';
+	// console.log('table:', table)
 	$('#table').append(table);
-	$('#date').datepicker({
-		changeMonth: true,
-		changeYear: true,
-		dateFormat: 'dd-mm-yy',
-		dayNames: ['Diumenge', 'Dilluns', 'Dimarts', 'Dimecres', 'Dijous', 'Divendres', 'Dissabte']
-	});
+
+	// Datepicker stuff
+	$.datepicker.setDefaults(
+		$.datepicker.regional['ca'],
+	)
+	
+	$('input[name=date]').each(function (_, elem) {
+		$(elem).datepicker(
+			{
+				changeMonth: true,
+				changeYear: true,
+				dateFormat: 'dd-mm-yy',
+				dayNames: ['Diumenge', 'Dilluns', 'Dimarts', 'Dimecres', 'Dijous', 'Divendres', 'Dissabte']
+			}
+		)
+		//$(elem).datepicker('setDate', $(elem).value)
+	})
+
+	$('input').on('change', function () {
+		if ($('#update').is(':disabled')) {
+			$('#update').attr('disabled', false)
+		}
+	})
+}
+
+function sendUpdate() {
+	const updatedData = new FormData
 }
 
 function reqError(err) {
@@ -66,17 +103,17 @@ $('#fetchAll').click(function () {
 })
 
 
-$.ajax({
-	type: 'POST',
-	url: 'http://localhost:3000/api/afegir',
-	contentType: 'application/json',
-	data: JSON.stringify({
-		id: 12,
-		nom: "CSGO",
-		preu: 12.0,
-		clase_preu: "gratis",
-		data: "2012-04-23T18:25:43.511Z",
-		genere: "Shooter"
-	})
-})
+// $.ajax({
+	// type: 'POST',
+	// url: 'http://localhost:3000/api/afegir',
+	// contentType: 'application/json',
+	// data: JSON.stringify({
+		// id: 12,
+		// nom: "CSGO",
+		// preu: 12.0,
+		// clase_preu: "gratis",
+		// data: "2012-04-23T18:25:43.511Z",
+		// genere: "Shooter"
+	// })
+// })
 
---
+++ frontend/js/objects.js
@@ -0,0 +1,127 @@
+class ClasePreu {
+	// Enum-like class
+	static gratis = new ClasePreu(0, 'gratis', 0, 0);
+	static barat = new ClasePreu(1, 'barat', 0.01, 7.99);
+	static mitja = new ClasePreu(2, 'mitjà', 8, 24.99);
+	static car = new ClasePreu(3, 'car', 25, 1000);
+	
+	constructor(id, type, from, to) {
+		this.id = id;
+		this.type = type;
+		this.range = {'from': from, 'to': to};
+	}
+	
+	toString() {
+		return `${this.type}`;
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	set id(id) {
+		this._id = id;
+	}
+
+	get type() {
+		return this._type;
+	}
+
+	set type(type) {
+		this._type = type
+	}
+
+	get range() {
+		return this._range;
+	}
+
+	set range({from, to}) {
+		this._range = { "from": from, "to": to };
+	}
+}
+
+class Genere {
+	constructor(genere) {
+		this.genere = genere;
+	}
+
+	toString() {
+		return `${this.genere}`;
+	}
+
+	get genere() {
+		return this._genere;
+	}
+
+	set genere(genere) {
+		this._genere = genere;
+	}
+}
+
+class Game {
+	constructor(id, nom, preu, clase_preu, genere, dataLlan) {
+		this.id = id;
+		this.nom = nom;
+		this.preu = preu;
+		this.clase_preu = preu;
+		this.genere = genere;
+		this.dataLlan = dataLlan;
+	}
+
+	set id(id) {
+		this._id = id;
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	set nom(nom) {
+		this._nom = nom;
+	}
+
+	get nom() {
+		return this._nom;
+	}
+
+	set preu(preu) {
+		this._preu = preu;
+	}
+
+	get preu() {
+		return this._preu;
+	}
+
+	set clase_preu(preu) {
+		const objk = Object.keys(ClasePreu);
+		for (const gt in objk) {
+			if (ClasePreu[objk[gt]].range.to > preu && ClasePreu[objk[gt]].range.from < preu) {
+				this._clase_preu = ClasePreu[objk[gt]];
+			}
+		}
+	}
+
+	get clase_preu() {
+		return this._clase_preu;
+	}
+
+	set genere(genere) {
+		this._genere = new Genere(genere)
+	}
+
+	get genere() {
+		return this._genere;
+	}
+
+	set dataLlan(data) {
+		this._dataLlan = new Date(data);
+	}
+
+	get dataLlan() {
+		return this._dataLlan.toISOString().slice(0,10);
+	}
+
+	toString() {
+		return `${this.nom} va ser llançat el ${this.dataLlan}. És un joc ${this.genere} i té un preu ${this.clase_preu}, de ${this.preu}`;
+	}
+}
